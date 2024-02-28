f49895472dffd8dfd5c332982bb5b92be987fe8a
ZTStew <zt.stewart.5@gmail.com>
2024-02-28 11:34:41 -0500

    Base requirements set

---
+++ README.md
@@ -0,0 +1,16 @@
+# Python Recursive Deleter
+---
+## Description:  
+Program is intended to be given a file name after which the program will recursively search every folder, with the execution point being the root, for the given file
+
+Program should ignore any folders with the name: `Backup`
+Program should not look at the files in the execution root
+
+Program should be written to read a `blacklist` (.json?) file that contains a list of file names user would like to delete
+
+---
+### Usage:
+(explanation of how to use the program)
+1. Navigate to the root folder that you want to delete files from
+2. Execute `re_del.py`
+3. Input the name of the file you would like to delete
---
+++ re_del.py
@@ -0,0 +1,41 @@
+"""
+Description:
+  Program is intended to be given a file name after which the program will recursively search every folder, with the execution point being the root, for the given file
+
+  Program should ignore any folders with the name: `Backup`
+  Program should not look at the files in the execution root
+"""
+
+import os, argparse
+import logging as log
+
+path = os.path.dirname(os.path.abspath(__file__)) + '\\Log\\recursive_deleter.log'
+
+log.basicConfig(
+    filename= path,
+    level=log.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+# log.debug("debug message")
+# log.info("info message")
+# log.warning("warning message")
+# log.error("error message")
+# log.critical("critical message")
+
+log.critical("### ### ### V Program Starts V ### ### ###")
+
+args = argparse.ArgumentParser()
+args.add_argument(
+  "-t",
+  "--test",
+  type=int,
+  help="Declair if the application should run in test mode [0 -> production (default) | 1 -> test mode]."
+)
+
+args = args.parse_args()
+
+if args.test:
+  log.critical("Test Running")
+
+
+log.critical("Program Terminated")
