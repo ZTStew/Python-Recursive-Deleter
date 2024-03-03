c78222ae35eeba77314a11639a11fe390e833f27
ZTStew <zt.stewart.5@gmail.com>
2024-03-03 02:34:16 -0500

    Added method for identifying folders in ignore

--- re_write.py
+++ re_write.py
@@ -1,12 +1,19 @@
 """
 Description:
   Program is intended to be the inverse of `re_del.py` creating the test files to be deleted by `re_del.py`
+
+# Rejected method for searching folders for the reason of it searching hidden folders
+# recursively searches all directories returning (dirpath, dirnames, filenames)
+# for i in os.walk("./"):
+#   print(i[0])
 """
 
 import os, argparse
 import logging as log
+from glob import glob
 
-ignore = [".git", "Backup"]
+# Ignored files must use following format so that the root directory can be added to ignore
+ignore = [".\\", ".\\Backup\\", ".\\log\\"]
 
 path = os.path.dirname(os.path.abspath(__file__)) + '\\Log\\recursive_writer.log'
 
@@ -36,12 +43,28 @@ args = args.parse_args()
 if args.test:
   log.critical("Test Running")
 
+folders = glob("./**/", recursive = True)
 
-# recursively searches all directories returning (dirpath, dirnames, filenames)
-# for i in os.walk("./"):
-#   print(i[0])
+print(folders)
 
-# from glob import glob
-# print(glob("./**/", recursive = True))
+# Searches `folders` for directories that are not included in `ingore`
+for i in folders:
+  blacklist_flag = False
+  # loops through ignore array
+  for j in ignore:
+    # print(j)
+    if j == i:
+      blacklist_flag = True
+      break
+
+  if blacklist_flag:
+    log.info("Ignored Folder: " + i)
+    print("Ignored Folder: " + i)
+  else:
+    log.info("Identified Folder: " + i)
+    print("Identified Folder: " + i)
+
+
+
 
 log.critical("Program Terminated")
